68e023e27631039c6c795a740a839932bd8197b5
groot-17 <100340433+groot-17@users.noreply.github.com>
2023-06-23 11:01:04 +0530

    Add files via upload

--- StudentTest.java
+++ StudentTest.java
@@ -1,25 +1,35 @@
-package com.practice.example;
+package com.mbdri;
 
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class StudentTest {
-	
 	public static void main(String[] args) {
+		List<Student> s1=new ArrayList<>();
 		
-		Set<Student> s1 = new TreeSet<Student>();
+		s1.add(new Student(1,"vipin",99,23));
+		s1.add(new Student(2,"vivek",84,23));
+		s1.add(new Student(3,"kajal",73,23));
+		s1.add(new Student(4,"priya",83,23));
+		s1.add(new Student(5,"kshitiz",65,23));
+		s1.add(new Student(6,"vaibhav",99,23));
+		s1.add(new Student(7,"sourabh",76,23));
+		s1.add(new Student(8,"tavneet",72,23));
+		s1.add(new Student(9,"suraj",60,23));
 		
-		s1.add(new Student(101, "Kshitiz", 20));
-		s1.add(new Student(103, "dev", 22));
-		s1.add(new Student(102, "suraj", 21));
-		s1.add(new Student(104, "sourabh", 19));
+	System.out.println(s1.stream().filter(s->s.getMarks()<=80).collect(Collectors.toList()));
+//Map<Integer, Integer> map=	s1.stream().filter(c->c.getMarks()<=80).collect(Collectors.groupingBy(LinkedHashMap,Student::getRollNo,Student::getMarks));
+	
+LinkedHashMap<Integer, Integer> map=	s1.stream().filter(c->c.getMarks()<=80).collect(Collectors.toMap(Student::getRollNo, Student::getMarks,(marks1,marks2)->marks2,LinkedHashMap::new));
+System.out.println(map);
 		
-		System.out.println(s1);
 		
+			
 		
 	}
-
-	
-	
 
 }
