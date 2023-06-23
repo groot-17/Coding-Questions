38901a6a8ada645259fdbd3dcd0b279931aead99
groot-17 <100340433+groot-17@users.noreply.github.com>
2023-06-23 10:57:53 +0530

    Add files via upload

---
+++ Coding.java
@@ -0,0 +1,27 @@
+package com.mbdri;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.practice.example.New;
+
+public class Coding {
+	public static void main(String[] args) {
+		String s1= "Communication";
+		String[] s2= {"ram","raj","ram","mohan","raj"};
+//		System.out.println("Reverse of String");
+//		System.out.println(Stream.of(s1).map(s->new StringBuilder(s).reverse()).collect(Collectors.joining()));
+//		System.out.println("String map");
+//		System.out.println(Arrays.stream(s1.split("")).map(String::toLowerCase).collect(Collectors.groupingBy(s->s,LinkedHashMap::new,Collectors.counting())));
+		//System.out.println("Map");
+	//	System.out.println(Stream.of(s1).map(String::toLowerCase).collect(Collectors.groupingBy(s->s,Collectors.counting())));
+		
+		System.out.println(Arrays.stream(s1.split("")).map(String::toLowerCase).collect(Collectors.groupingBy(s->s,LinkedHashMap::new,Collectors.counting())));
+		System.out.println(Stream.of(s2).collect(Collectors.groupingBy(s->s,LinkedHashMap::new,Collectors.counting())));
+		System.out.println(Stream.of(s1).map(s->new StringBuilder(s1).reverse()).collect(Collectors.joining()));
+	}
+
+}
